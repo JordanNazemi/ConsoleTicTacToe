a9a4d75f799900b93b2e578c1f386f04a09a4e2f
jnazemi <jnazemi@luc.edu>
2019-11-13 18:59:48 -0600

    Initial upload

---
+++ TicTacToe.py
@@ -0,0 +1,76 @@
+def create_board(size):
+    board = ""
+    space = 1
+    for x in range(0, size):
+        for x in range(0, size):
+            board += (str(space) + "\t")
+            space += 1
+        board += "\n"
+    return board
+
+
+def add_piece(piece, space, board):
+    new_board = board.replace(str(space), piece)
+    return new_board
+
+
+def check_winner(board):
+    rows = board.splitlines()
+    for index, row in enumerate(rows):
+        rows[index] = row.replace("\t", "")
+
+    # First check see if won by rows
+    for row in rows:
+        for character in row:
+            winner = True
+            if (row[0] != character):
+                winner = False
+        if winner:
+            return True
+
+    # Now columns
+    for column in range(0, len(rows[0])):
+        for row in range(1, len(rows[0])):
+            winner = True
+            if rows[row][column] != rows[0][column]:
+                winner = False
+        if winner:
+            return True
+
+    # Now Diagnols
+    # TODO
+    return winner
+
+
+def check_full(board):
+    board = board.replace("\n", "")
+    board = board.replace("\t", "")
+
+    for x in board:
+        if x != "X" and x != "O":
+            return False
+    return True
+
+
+winner = False
+turn = "X"
+
+print("How large would you like the board?")
+size = int(input())
+board = create_board(size)
+
+while not winner:
+
+    print(board)
+    print(f"Player {turn} choose a space!")
+
+    space = input()
+    board = add_piece(turn, space, board)
+
+    winner = check_full(board)
+    winner = check_winner(board)
+
+    if turn == "X":
+        turn = "O"
+    else:
+        turn = "X"
